884887c83c94df49932c1cbf1f8dd3ada67b06c5
V-Taylor <taylor.vingadassalon@u-psud.fr>
2017-04-29 04:42:01 +0200

    res_rmsd ok + déoupage modulaire du parserPDB (atmW séparé)

--- rmsd.py
+++ rmsd.py
@@ -2,28 +2,10 @@
 # -*- coding: utf-8 -*-
 #
 #  rmsd.py
-#  
-#  Copyright 2017 Taylor <taylor@taylor-PC>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+
 import sys
 import math
-import structureTools_TaylorVingadassalon as st
+import structureTools_TaylorArnaud as st
 
 
 ## Prend en entrée deux dictionnaires de protéines et calcule le rmsd entre ces deux protéines
@@ -46,20 +28,28 @@ def rmsd(a,b,liste=['CA']): #Atome par défaut CA
 ## Prend en entrée deux dictionnaires de protéines et calcule le rmsd entre chaque résidus des protéines 
 ## (sortie : colonne 1 position du résidus; colonne 2: RMDS entre les deux conf)
 def res_rmsd(a,b,liste=['CA']): #Atome par défaut CA
-	N = 0
-	res = 0
-	s=0.0
+
+	nbRes = 0
+	dicoRmsd = dict()
+
 	for j in a.keys():# dans les chaines
 		for k in a[j].keys(): # dans les résidus
-			res+=1
-			for l in a[j][k].keys(): #dans les dictionnaires d'info
-				
-				#~ if(l in liste): #On ignore le centre de masse s'il y en a un
-					#~ s+=pow(st.distanceAtomes(a[j][k][l],b[j][k][l]),2)
-					#~ N+=1
+			
+			## pour chaque résidu k, on calcul le rmsd des atomes de ce résidu et on le stoque dans un dictionnaire
+			nbRes+=1; N = 0; s=0.0;
+			
+			for l in a[j][k].keys(): #dans les dictionnaires d'info de chaque atome
+				if(l in liste): #Si l'atome fait partie de la liste des atomes sur lesquels on calcule le rmsd:
+					s+=pow(st.distanceAtomes(a[j][k][l],b[j][k][l]),2)
+					N+=1
+					
+			#  Si N>0 : Au moins 1 paire d'atome alignée pour le résidu : calcul et stockage du rmsd 
+			if(N>0):
+				dicoRmsd[k]=math.sqrt(s/N) #Sauvegarde du RMSD du résidu k
+		
 	print("Paires d'atomes alignées : ",N)
-	print("Nombre résidus: ",res)
-	return(math.sqrt(s/N))	
+	print("Nombre résidus: ",nbRes)
+	return(dicoRmsd)	
 
 ## Fonction qui permettra d'afficher le graphique RMDS vs AA positions (pas de dictionnaire en entrée car temporalité importante) (liste probablement)
 def drawRMDS(a):
@@ -83,5 +73,9 @@ if __name__ == '__main__':
 	dicoProt2 = st.lirePDB(prot2,ficAtome)
 	
 	#Calculer rmsd 
-	print(rmsd(dicoProt1,dicoProt2,['CA','N','O']))
+	dicTest = res_rmsd(dicoProt1,dicoProt2,['CA','N','O'])
+	for a in dicTest.keys():
+		print(a,"	",dicTest[a])
+	
+	print(len(dicTest))
 	
--- structureTools_TaylorArnaud.py
+++ structureTools_TaylorArnaud.py
@@ -22,41 +22,42 @@ def lireAtoms(a):
 	return atome
 	
 
+
 ## Retourne le dictionnaire correspondant au fichier PDB lu
 # @a : chemin du fichier PDB
 # @b : dictionnaire atomes-masse moleculaire
-def lirePDB(a,b):
+def lirePDB(a):
 
-	
 	#### Dictionnaires ####
 	residu = dict()
 	chaine = dict()
 	atome = dict()
-	atmWeight = lireAtoms(b)
 	#######################
 	
-
 	with open(a, "r") as fichier:
-		fic = fichier.readlines()
 		conf = ''
+		fic = fichier.readlines()
+
 		for l in fic:
-			
+
 			if (conf=='' and l[0:6].strip()=='ATOM'): #Pour la première ligne du fichier, l'ascii du char[0]=65279 parfois indique le début d'une zone de texte
-				conf=l[16]
+				conf=l[17]
 				
-			if (l[0:4]=="ATOM" and l[16]==conf): #Scan des chaines d'interet
+			if (l[0:4]=="ATOM" and l[17]==conf): #Scan des chaines d'interet
 				##### Recuperation des donnees ###
-				res=l[17:20].strip()
-				nomAtome = l[13:16].strip()
+				res=l[22:26].strip()
+				nomAtome = l[12:16].strip()
 				chName=l[21].strip()
-				
-				info={'ID': (l[7:11].strip()),
-						'x'	: (float)(l[31:38].strip()),
-						'y' : (float)(l[39:46].strip()),
-						'z' : (float)(l[47:54].strip()),
-						'atmW': atmWeight[l[77].strip()]
+
+				info={'ID': (l[6:11].strip()),
+						'x'	: (float)(l[30:38].strip()),
+						'y' : (float)(l[38:46].strip()),
+						'z' : (float)(l[46:54].strip()),
 					}
 				######## Fin recuperation ########
+				
+				#~ if nomAtome =='P':
+					#~ print("Trouvé:",info['ID'])
 				
 				 # Chaine non repertoriee donc on l'ajoute
 				if (chName not in chaine.keys()):
@@ -70,7 +71,29 @@ def lirePDB(a,b):
 
 	return chaine
 
-
+# 
+def selectElement(atomeName):
+	atomeName=atomeName.strip()
+	courant = ['C','H','O','N','P']
+	if atomeName[0] in courant:
+		return atomeName[0]
+	else:
+		print("Un atome non répertorié utilisé !")
+	
+	
+#Ajoute au dictionnaire info (pour chaque atome) la masse de l'atome 'atmW'
+def addAtomWeight(pathAtomes, dicoPDB):
+	masseAtomes = lireAtoms(pathAtomes)
+	for chaine in dicoPDB.keys():
+		for residu in dicoPDB[chaine].keys():
+			for atome in dicoPDB[chaine][residu].keys():
+				elem = selectElement(atome)
+				if(elem == None): # Si un atome non répertorié est trouvé on l'affiche
+					print(chaine,'	',residu,"	","\'",atome,"\'","	",elem)
+				else: # Le nom de l'élément a été récupéré, on ajoute sa masse
+					dicoPDB[chaine][residu][atome]['atmW']=masseAtomes[elem]
+	
+	
 ## Ajoute pour chaque residu du dictionnaire b la position (x,y,z) de son centre de masse
 # @a: dictionnaire issu de la commande lirePDB
 def ajouterCentreDeMasse(a):
@@ -161,19 +184,22 @@ def createPDB(a):
 	
 if __name__ == '__main__':
 	monDico = dict()
-	ficAtome=sys.argv[1]
-	prot1 =sys.argv[2]
-	prot2 =sys.argv[3]
+	ficAtome=sys.argv[3]
+	prot1 =sys.argv[1]
+	prot2 =sys.argv[2]
 	
-	dicoProt1 = lirePDB(prot1,ficAtome)
-	dicoProt2 = lirePDB(prot2,ficAtome)
+	dicoProt1 = lirePDB(prot1)
+	dicoProt2 = lirePDB(prot2)
 	
-	ajouterCentreDeMasse(dicoProt1)
-	ajouterCentreDeMasse(dicoProt2)
+	#~ ajouterCentreDeMasse(dicoProt1)
+	#~ ajouterCentreDeMasse(dicoProt2)
 
+	addAtomWeight(ficAtome,dicoProt1)
+	print(dicoProt1)
 	
 	#~ mat = distance(monDico)
 	#~ print(dicoProt1)
 	#~ print(dicoProt2)
+
 
 
